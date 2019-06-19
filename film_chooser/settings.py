64a47da118bf1705bf0ed2e79dad4dd64fd3fcfd
nickolay.osiniy <nikolaj.osinij@privatbank.ua>
2019-06-19 15:03:57 +0300

    added facebook auth

--- film_chooser/settings.py
+++ film_chooser/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
 
     'chooser',
     'users'
