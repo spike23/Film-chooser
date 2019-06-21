31fc77ece28b1814396056e4148cc5e583cb69d4
nickolay.osiniy <nikolaj.osinij@privatbank.ua>
2019-06-21 17:04:34 +0300

    intital model

--- chooser/models.py
+++ chooser/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from users.models import CustomUser
 
 
 class FilmsBase(models.Model):
     films = models.CharField(max_length=100)
+    last_updated = models.DateField(auto_now=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '{films}'.format(films=self.films)
+        return '{films}, {last_updated}'.format(films=self.films, last_updated=self.last_updated)
 
 
 class FilmsToWatching(models.Model):
     films = models.CharField(max_length=100)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return '{films}'.format(films=self.films)
