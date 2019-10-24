99a998e6c384bc50c0a83b7721bf50fc9ac1376d
nickolay.osiniy <nikolaj.osinij@privatbank.ua>
2019-10-24 13:12:57 +0300

    fixed static_root

--- film_chooser/settings.py
+++ film_chooser/settings.py
@@ -129,9 +129,11 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
