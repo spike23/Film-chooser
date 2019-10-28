25ff416d163cb853293a5d92961911ab74078122
nickolay.osiniy <nikolaj.osinij@privatbank.ua>
2019-10-28 15:35:28 +0200

    change static root filepath

--- film_chooser/settings.py
+++ film_chooser/settings.py
@@ -128,10 +128,11 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'stat_ext')
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
