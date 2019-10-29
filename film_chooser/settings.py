0b110bfe56084d4734b1903df9e56fb393260420
nickolay.osiniy <nikolaj.osinij@privatbank.ua>
2019-10-29 15:31:24 +0200

    fork on postgres

--- film_chooser/settings.py
+++ film_chooser/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+
+import dj_database_url
 from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
@@ -88,10 +90,9 @@ WSGI_APPLICATION = 'film_chooser.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': dj_database_url.config(default=config('SQLITE')),
+    'default': dj_database_url.config(default=config('POSTGRES'))
+
 }
 
 # Password validation
