79e68112379875d570c3aac4d07723342fb42cf5
nickolay.osiniy <nikolaj.osinij@privatbank.ua>
2019-11-12 10:09:17 +0200

    refactoring films views

--- films/urls.py
+++ films/urls.py
@@ -1,15 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from films.views import SearchFilmsBaseView, SearchFilmsWatchingView, SearchMainView
-from . import views
+from films.views import (SearchFilmsBaseView, SearchFilmsWatchingView, SearchMainView, AddNewFilmView,
+                         WatchingFilmListView, BaseFilmListView, EditFilmView, DeleteFilmsBaseView,
+                         DeleteFilmsWatchingView)
 
 urlpatterns = [
-    path('film-list/', views.base_film_list, name='base_film_list'),
-    path('watching-list/', views.watching_film_list, name='watching_film_list'),
-    path('add-list-base/', views.add_new_film, name='add_new_film'),
-    path('edit-film/<int:pk>/', views.edit_film, name='edit_film'),
-    path('delete-list-base/', views.delete_films_base, name='delete_films_base'),
-    path('delete-list-watching/', views.delete_films_watching, name='delete_films_watching'),
+    path('film-list/', login_required(BaseFilmListView.as_view()), name='base_film_list'),
+    path('watching-list/', login_required(WatchingFilmListView.as_view()), name='watching_film_list'),
+    path('add-list-base/', login_required(AddNewFilmView.as_view()), name='add_new_film'),
+    path('edit-film/<int:pk>/', login_required(EditFilmView.as_view()), name='edit_film'),
+    path('delete-list-base/', login_required(DeleteFilmsBaseView.as_view()), name='delete_films_base'),
+    path('delete-list-watching/', login_required(DeleteFilmsWatchingView.as_view()), name='delete_films_watching'),
     path('film-list-search/', SearchFilmsBaseView.as_view(), name='search_film_base_results'),
     path('film-watching-search/', SearchFilmsWatchingView.as_view(), name='search_film_watching_results'),
     path('film-search/', SearchMainView.as_view(), name='search_results')
--- films/views.py
+++ films/views.py
@@ -1,115 +1,126 @@
 from itertools import chain
 
-from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView, DeleteView
 
 from chooser.forms import NewFilmForm
 from chooser.models import FilmsBase, FilmsToWatching
 from premieres.models import PremierList
 
-film_list_template = 'films/film_list.html'
-watching_list_template = 'films/watching_list.html'
-edit_film_template = 'films/edit_film.html'
 
+class BaseFilmListView(ListView):
+    model = FilmsBase
+    template_name = 'films/film_list.html'
 
-@login_required
-def base_film_list(request):
-    current_user = request.user.id
-    films = FilmsBase.objects.filter(user_id=current_user).order_by('films')
-    paginator = Paginator(films, 7)
-    page = request.GET.get('page')
-    film_list = paginator.get_page(page)
+    def dispatch(self, request, *args, **kwargs):
+        current_user = request.user.id
+        films = FilmsBase.objects.filter(user_id=current_user).order_by('films')
+        paginator = Paginator(films, 7)
+        page = request.GET.get('page')
+        film_list = paginator.get_page(page)
 
-    saved_films_premieres = request.session.get('saved_films')
-
-    context = {
-        'films': film_list,
-        'saved_films': saved_films_premieres
-    }
-
-    if request.session.get('saved_films'):
-        del request.session['saved_films']
-
-    return render(request, film_list_template, context)
-
-
-@login_required
-def watching_film_list(request):
-    current_user = request.user.id
-    films = FilmsToWatching.objects.filter(user_id=current_user).order_by('films')
-    paginator = Paginator(films, 7)
-    page = request.GET.get('page')
-    film_list = paginator.get_page(page)
-
-    context = {
-        'films': film_list
-    }
-
-    return render(request, watching_list_template, context)
-
-
-@login_required
-def delete_films_base(request):
-    current_user = request.user.id
-    values = request.POST.getlist('delete')
-    FilmsBase.objects.filter(pk__in=values, user_id=current_user).delete()
-    return redirect('base_film_list')
-
-
-@login_required
-def delete_films_watching(request):
-    current_user = request.user.id
-    values = request.POST.getlist('delete')
-    FilmsToWatching.objects.filter(pk__in=values, user_id=current_user).delete()
-    return redirect('watching_film_list')
-
-
-@login_required
-def add_new_film(request):
-    current_user = request.user.id
-    form = NewFilmForm(request.POST)
-    films = FilmsBase.objects.filter(user_id=current_user).order_by('films')
-    paginator = Paginator(films, 7)
-    page = request.GET.get('page')
-    film_list = paginator.get_page(page)
-    if request.method == 'POST' and form.is_valid():
-        film = form.cleaned_data.get('new_film')
-        FilmsBase.objects.update_or_create(films=film, user_id=current_user)
+        saved_films_premieres = request.session.get('saved_films')
 
         context = {
-            'form': form,
-            'new_film': film,
+            'films': film_list,
+            'saved_films': saved_films_premieres
+        }
+
+        if request.session.get('saved_films'):
+            del request.session['saved_films']
+
+        return render(request, self.template_name, context)
+
+
+class WatchingFilmListView(ListView):
+    model = FilmsToWatching
+    template_name = 'films/watching_list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        current_user = request.user.id
+        films = FilmsToWatching.objects.filter(user_id=current_user).order_by('films')
+        paginator = Paginator(films, 7)
+        page = request.GET.get('page')
+        film_list = paginator.get_page(page)
+
+        context = {
             'films': film_list
         }
 
-        return render(request, film_list_template, context)
+        return render(request, self.template_name, context)
 
 
-@login_required
-def edit_film(request, pk):
-    current_user = request.user.id
-    form = NewFilmForm(request.POST)
-    edited = FilmsBase.objects.get(user_id=current_user, pk=pk)
+class DeleteFilmsBaseView(DeleteView):
+    model = FilmsBase
 
-    films = FilmsBase.objects.filter(user_id=current_user).order_by('films')
-    paginator = Paginator(films, 7)
-    page = request.GET.get('page')
-    film_list = paginator.get_page(page)
+    def dispatch(self, request, *args, **kwargs):
+        current_user = request.user.id
+        values = request.POST.getlist('delete')
+        FilmsBase.objects.filter(pk__in=values, user_id=current_user).delete()
+        return redirect('base_film_list')
 
-    if request.method == 'POST' and form.is_valid():
-        film = form.cleaned_data.get('new_film')
-        edited.films = film
-        edited.save()
 
-    context = {
-        'film': edited,
-        'films': film_list
-    }
+class DeleteFilmsWatchingView(DeleteView):
+    model = FilmsToWatching
 
-    return render(request, edit_film_template, context)
+    def dispatch(self, request, *args, **kwargs):
+        current_user = request.user.id
+        values = request.POST.getlist('delete')
+        FilmsToWatching.objects.filter(pk__in=values, user_id=current_user).delete()
+        return redirect('watching_film_list')
+
+
+class AddNewFilmView(UpdateView):
+    model = FilmsBase
+    template_name = 'films/film_list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        current_user = request.user.id
+        form = NewFilmForm(request.POST)
+        films = FilmsBase.objects.filter(user_id=current_user).order_by('films')
+        paginator = Paginator(films, 7)
+        page = request.GET.get('page')
+        film_list = paginator.get_page(page)
+        if request.method == 'POST' and form.is_valid():
+            film = form.cleaned_data.get('new_film')
+            FilmsBase.objects.update_or_create(films=film, user_id=current_user)
+
+            context = {
+                'form': form,
+                'new_film': film,
+                'films': film_list
+            }
+
+            return render(request, self.template_name, context)
+
+
+class EditFilmView(UpdateView):
+    model = FilmsBase
+    template_name = 'films/edit_film.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        current_user = request.user.id
+        form = NewFilmForm(request.POST)
+        edited = FilmsBase.objects.get(user_id=current_user, pk=self.kwargs.get('pk'))
+
+        films = FilmsBase.objects.filter(user_id=current_user).order_by('films')
+        paginator = Paginator(films, 7)
+        page = request.GET.get('page')
+        film_list = paginator.get_page(page)
+
+        if request.method == 'POST' and form.is_valid():
+            film = form.cleaned_data.get('new_film')
+            edited.films = film
+            edited.save()
+
+        context = {
+            'film': edited,
+            'films': film_list
+        }
+
+        return render(request, self.template_name, context)
 
 
 class SearchFilmsBaseView(ListView):
@@ -152,14 +163,14 @@ class SearchMainView(ListView):
 
         if query is not None:
             film_base = FilmsBase.objects.filter(
-            Q(films__icontains=query)
-        )
+                Q(films__icontains=query)
+            )
             watching_film = FilmsToWatching.objects.filter(
-            Q(films__icontains=query)
-        )
+                Q(films__icontains=query)
+            )
             premieres = PremierList.objects.filter(
-            Q(films__icontains=query)
-        )
+                Q(films__icontains=query)
+            )
             queryset_chain = chain(
                 film_base,
                 watching_film,
